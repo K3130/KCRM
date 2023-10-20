979d66beed6e701405588bd4f0dd193806f666a6
Konstantin Smirnov <23383424+K3130@users.noreply.github.com>
2023-10-20 15:18:17 +0300

    add class

---
+++ src/data_base/data_base_manager_kcrm.cpp
@@ -0,0 +1,6 @@
+#include "data_base_manager_kcrm.h"
+
+data_base_manager_kcrm::data_base_manager_kcrm()
+{
+
+}
---
+++ src/data_base/data_base_manager_kcrm.h
@@ -0,0 +1,13 @@
+#ifndef DATA_BASE_MANAGER_KCRM_H
+#define DATA_BASE_MANAGER_KCRM_H
+
+#include <QObject>
+
+class data_base_manager_kcrm : public QObject
+{
+    Q_OBJECT
+public:
+    data_base_manager_kcrm();
+};
+
+#endif // DATA_BASE_MANAGER_KCRM_H
